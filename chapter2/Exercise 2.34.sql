9560175dc9e6f8e9c945d6b72d81d17bd6b54d2b
Eddy-star223 <eddyeffanga@gmail.com>
2025-08-08 01:03:12 +0100

    MySQL Assignment

---
+++ chapter2/Exercise 2.34.sql
@@ -0,0 +1,14 @@
+select sum(QuantityOnHand)
+from inventory;
+
+select max(QuantityOnHand)
+from inventory;
+
+select min(QuantityOnHand)
+from inventory;
+
+select avg(QuantityOnHand)
+from inventory;
+
+select count(QuantityOnHand)
+from inventory;
---
+++ chapter2/Exercise 2.35.sql
@@ -0,0 +1,5 @@
+count() is use to count the number of rows.
+
+while
+
+sum() is use to add up (sums) the values in a numeric column.
